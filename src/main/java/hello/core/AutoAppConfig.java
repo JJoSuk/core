0bc3047a5c9bfc80629d647b98c2c24d16f98ee3
joseoghwan <loveyou55042@gmail.com>
2023-05-29 23:42:11 +0900

    Changes

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,33 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.order.OrderService;
+import hello.core.discount.order.OrderServiceImpl;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+//    @Autowired MemberRepository memberRepository;
+//    @Autowired
+//    DiscountPolicy discountPolicy;
+//
+//    @Bean
+//    OrderService orderService() {
+//        return new OrderServiceImpl(memberRepository, discountPolicy);
+//    }
+
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+}
---
+++ src/main/java/hello/core/discount/order/OrderServiceImpl.java
@@ -0,0 +1,43 @@
+package hello.core.discount.order;
+
+import hello.core.annotation.MainDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+//@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // @RequiredArgsConstructor 를 만들면 아래 생성자를 자동으로 만들어준다
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+}
